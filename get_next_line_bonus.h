449a96d92b32697f878c555d9cb59e2193a17923
Amine Douyahia <114682049+Amdouyah@users.noreply.github.com>
2022-12-22 23:05:30 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdouyah <amdouyah@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 22:58:59 by amdouyah          #+#    #+#             */
+/*   Updated: 2022/12/20 02:16:36 by amdouyah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+size_t	ft_str_len(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_get_line(char *save);
+char	*ft_save(char *save);
+int		ft_str_chr(char *s, int c);
+char	*ft_read(int fd, char *r);
+
+#endif
